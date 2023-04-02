29593853b2c3529dccbb9ee1b2f565b6af6c5bf1
SojibRWD <sajibhashi1@gmail.com>
2023-04-02 12:58:05 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -80,6 +80,27 @@ $('.about_slick').slick({
     ]
   });
 
+  // ====back to top start js====
+
+  $(window).on('scroll', function(event) {
+      if($(this).scrollTop() > 600){
+          $('.back_to_top').fadeIn(200)
+      } else{
+          $('.back_to_top').fadeOut(200)
+      }
+  });
+  //==== Animate the scroll to top===
+  $('.back_to_top').on('click', function(event) {
+      event.preventDefault();
+  
+      $('html, body').animate({
+          scrollTop: 0,
+      }, 1000);
+  }); 
+  
+  // ====back to top end js====
+
+
 
 // count down part start 
   (function () {
